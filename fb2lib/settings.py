f8f64806dcaaf1d47a88aa59f78eaa3ddab2e088
andrew <fantazey@gmail.com>
2015-11-12 19:00:49 +0700

    Расширение настроек базы данных и путей

--- fb2lib/settings.py
+++ fb2lib/settings.py
@@ -22,6 +22,8 @@ DATABASES = {
     }
 }
 
+from private_settings import DATABASES
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
